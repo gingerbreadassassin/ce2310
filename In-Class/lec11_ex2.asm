2a118bdce31618461a48a4421b3149960c0a54d7
Connor Ness <connordvn@gmail.com>
2017-10-03 12:35:23 -0500

    added comments

--- In-Class/lec11_ex2.asm
+++ In-Class/lec11_ex2.asm
@@ -1,8 +1,8 @@
 	.data
-str: 	.asciiz "Hello, world!\n"
+str: 	.asciiz "Hello, world!\n"	# define str as "Hello, world!\n"
 	.text
-main:	la $a0,str
-	li $v0,4
-	syscall
-	li $v0,10
-	syscall
+main:	la $a0,str			# load str into a0
+	li $v0,4			# load print into v0
+	syscall				# execute instruction in v0
+	li $v0,10			# load end into v0
+	syscall				# execute instruction in v0
