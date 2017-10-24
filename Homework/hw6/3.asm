35e9f88691394a917be0913ee37ebc76a680974a
Connor Ness <connordvn@gmail.com>
2017-10-23 23:07:35 -0500

    hw6 is done

--- Homework/hw6/3.asm
+++ Homework/hw6/3.asm
@@ -5,4 +5,32 @@ num3:	.word 0x54
 num4:	.word 0x55
 
 	.text
-main:
+main:	li $t0,0
+	la $t1,num1
+	li $v0,11
+	j store
+
+store:	lw $s0,($t1)
+	jal push
+	addi $t0,$t0,1
+	addi $t1,$t1,4
+	beq $t0,4,prt
+	j store
+	
+prt:	jal pop
+	la $a0,($s0)
+	syscall
+	addi $t0,$t0,-1
+	beqz $t0,end
+	j prt
+
+push:	addi $sp,$sp,-4
+	sw $s0,($sp)
+	jr $ra
+	
+pop:	lw $s0,($sp)
+	addi $sp,$sp,4
+	jr $ra
+	
+end:	li $v0,10
+	syscall
--- Homework/hw6/4.asm
+++ Homework/hw6/4.asm
@@ -1,10 +1,29 @@
 	.text
-main:	
+main:	la $t0,v
+	lw $t1,v
+	li $t3,5
+
+load:	subi $t3,$t3,1
+	beqz $t3,close
+	addi $t0,$t0,4
+	lw $t2,($t0)
+	bgt $t2,$t1,load
+	move $t1,$t2
+	j load
+	
+close:	la $a0,ldr
+	li $v0,4
+	syscall
+	la $a0,($t1)
+	li $v0,34
+	syscall
+	li $v0,10
+	syscall
 	
 	.data
-v:	.word 0x6966avce
+v:	.word 0x6966abce
 w:	.word 0x89c7d12e
 x:	.word 0x7ff9348c
 y:	.word 0x445e279a
 z:	.word 0x123ac45d
-ld:	.asciiz "The smallest number = "
+ldr:	.asciiz "The smallest number = "
