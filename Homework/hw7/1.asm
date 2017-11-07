cb2ff5f6862d64e4c92bdbe9f444bff4c85d524a
Connor Ness <connordvn@gmail.com>
2017-11-07 17:00:04 -0600

    alternate hw7.2, added comments to .1

--- Homework/hw7/1.asm
+++ Homework/hw7/1.asm
@@ -1,38 +1,38 @@
 	.text
-main:	la $t0,num1
-	la $t7,num1
-comp: 	lw $t1,0($t0)
-	lw $t2,4($t0)
-	beqz $t2,print
-	ble $t1,$t2,count
-	jal rev
-	j comp
-count: 	addi $t0,$t0,4
-	j comp
+main:	la $t0,num1	# pointer to first word
+	la $t7,num1	# again
+comp: 	lw $t1,0($t0)	# load word at address
+	lw $t2,4($t0)	# load next word
+	beqz $t2,print	# if word = 0, we're done
+	ble $t1,$t2,count # #'s in order, skip
+	jal rev		# #'s not in order, swap
+	j comp		# 
+count: 	addi $t0,$t0,4	# increment pointer
+	j comp		# compare next two #'s
 	
-rev: 	sub $sp,$sp,4 
-	sw $ra,($sp) 
-	sw $t1,4($t0) 
+rev: 	sub $sp,$sp,4 	# decrement stack pointer
+	sw $ra,($sp) 	# store ra on stack
+	sw $t1,4($t0) 	# switch words
 	sw $t2,0($t0)
-	beq $t0,$t7,goback 
-	sub $t0,$t0,4
-	lw $t1,0($t0)
-	lw $t2,4($t0)
-	ble $t1,$t2,goback
-	jal rev
-goback:	addi $t0,$t0,4
-	lw $ra,($sp)
-	addi $sp,$sp,4
-	jr $ra
+	beq $t0,$t7,goback # if at first word, skip
+	sub $t0,$t0,4	# decrement pointer
+	lw $t1,0($t0)	# load word 1
+	lw $t2,4($t0)	# load word 2
+	ble $t1,$t2,goback # if in order, skip
+	jal rev		# not in order, swap
+goback:	addi $t0,$t0,4	# increment pointer
+	lw $ra,($sp)	# load from stack
+	addi $sp,$sp,4	# increment stack pointer
+	jr $ra		# jump to ra
 
-print:	li $t0,0
-ploop:	li $v0,1
-	lw $a0,num1($t0)
-	beqz $a0,done
+print:	li $t0,0	# $t0 is offset
+ploop:	li $v0,1	# print integer
+	lw $a0,num1($t0)# load word at offset addr
+	beqz $a0,done	# if word is 0, done
 	syscall
-	addi $t0,$t0,4
-	jal pnl
-	j ploop
+	addi $t0,$t0,4	# increment pointer
+	jal pnl		# print newline
+	j ploop		# print next number
 
 pnl:	li $a0,0xA
 	li $v0,11
--- Homework/hw7/2.asm
+++ Homework/hw7/2.asm
@@ -1,22 +1,22 @@
 	.data
-array:	.space 1600
+array:	.space 1600	# Array of 400 integers (4 bytes each) in 20X20 array
 
 	.text
-main:	li $t0,0
-	li $t1,1
-even:	beq $t0,1600,done
-	sw $t1,array($t0)
-	addi $t0,$t0,4
-	addi $t1,$t1,1
-	beq $t1,21,odd
-	j even
+main:	li $t0,0		# $t0 is pointer offset
+	li $t1,1		# $t1 is the value to store
+even:	beq $t0,1600,done	# if $t0 = 1600, we're done
+	sw $t1,array($t0)	# store $t1 in array
+	addi $t0,$t0,4		# increment array pointer
+	addi $t1,$t1,1		# increment value
+	beq $t1,21,odd		# go to next row
+	j even			# loop back
 
-odd:	beq $t0,1600,done
-	addi $t1,$t1,-1
-	sw $t1,array($t0)
-	addi $t0,$t0,4
-	beq $t1,1,even
-	j odd
+odd:	beq $t0,1600,done	# if $t0 = 1600, we're done
+	addi $t1,$t1,-1		# decrement value
+	sw $t1,array($t0)	# store value in array
+	addi $t0,$t0,4		# increment array pointer
+	beq $t1,1,even		# go to next row
+	j odd			# loop back
 	
 done:	li $v0,10
 	syscall
