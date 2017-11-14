22b7cc18e2fefadb7ed5ae0e508604e77f6f94ac
Connor Ness <connordvn@gmail.com>
2017-11-13 18:24:19 -0600

    lab 6 done, but without storing $ra to ($sp)

--- Labs/6.asm
+++ Labs/6.asm
@@ -2,10 +2,11 @@
 inval:	.asciiz "\nPlease enter a lowercase letter, or capital P to print the current string!\n"
 fin:	.asciiz	"We're out of space, here is the final string: "
 str:	.asciiz "abcdefghijklmnopqrstuvwxyz"
-sp:	.space 30
+	.space 30
 
 	.text
 main:	li $t0,0
+	li $t3,0
 	j notake
 
 read:	beq $t0,30,done
@@ -15,6 +16,32 @@ read:	beq $t0,30,done
 	bgt $v0,0x7A,notake
 	blt $v0,0x61,notake
 	addi $t0,$t0,1
+	li $t1,0
+ins:	lb $t2,str($t1)
+	blt $v0,$t2,trikld
+	beqz $t2,zed
+	addi $t1,$t1,1
+	j ins
+	
+trikld:	beqz $t2,triklu
+	addi $sp,$sp,-1
+	sb $t2,($sp)
+	addi $t1,$t1,1
+	addi $t3,$t3,1
+	lb $t2,str($t1)
+	j trikld
+
+triklu:	lb $t2,($sp)
+	sb $t2,str($t1)
+	addi $sp,$sp,1
+	addi $t1,$t1,-1
+	addi $t3,$t3,-1
+	bgtz $t3,triklu
+	sb $v0,str($t1)
+	j notake
+	
+zed:	sb $v0,str($t1)
+	j notake
 
 notake:	la $a0,inval
 	li $v0,4
